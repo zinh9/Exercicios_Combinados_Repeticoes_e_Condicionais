c8de682d3f62422a0d1b5d281a3373d6c9fb0514
zinh9 <127460831+zinh9@users.noreply.github.com>
2023-09-05 10:55:21 -0300

    Create Exe 3.py

---
+++ Exe 3.py
@@ -0,0 +1,23 @@
+import random
+
+def adivinha(num_aleatorio):
+    chute = []
+    i = 0
+    
+    while chute != num_aleatorio:
+        chute.append(int(input("Qual numero o programa escolheu: ")))
+        
+        if chute[i] == num_aleatorio:
+            print("PARABÉNS, VOCÊ GANHOU O JOGO!!!")
+            break
+        
+        elif chute[i] > num_aleatorio:
+            print("Um pouco menos")
+        
+        elif chute[i] < num_aleatorio:
+            print("Um pouco mais")
+        
+        i += 1
+
+num_aleatorio = random.randint(1, 100)
+adivinha(num_aleatorio)
